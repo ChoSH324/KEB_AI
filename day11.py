743154b42e3aa2253165deaa38f9fc9defe59731
choSH324 <12201304@inha.edu>
2025-02-24 13:41:41 +0900

    day11 pandas 03

--- day11.py
+++ day11.py
@@ -1,12 +1,28 @@
 import numpy as np
 import pandas as pd
 
-# s= pd.Series([1, 2, 3, 4])
-s = pd.Series([1, 2, 3, 4],index = ['a','b','c','d'])
-print(s)
+df = pd.DataFrame(
+    {"a" :[4, 7 , 10],
+     "b" : [5, 88, 11],
+     "c" : [16, 9, 12]},
+    index = [1, 2, 3]
+    )
+# print('df',df)
 
-s2 = pd.Series([99, 100, 98, 91, 92])
-s2_subset=s2[1:4]
-print(s2_subset)
-s2_mean=s2.mean()
-print(s2_mean)
+# print(pd.melt(df))
+# df2=(pd.melt(df).rename(columns={'variable':'var','value':'val'}).query('val > 10').sort_values('val'))
+# print(df2)
+
+# df3 = df.iloc[1:2]
+df3 = df.iloc[:,[0,2]]
+print(df3)
+
+df4=df.loc[1:2]
+print(df4)
+
+def square(n)->int:
+    return n*n
+
+
+print(df.apply(square))
+# print(df.apply(lambda x : x* x))
